9a7dfc19285cf7fde26d087aab0fc289a1173911
Luke Bennett <luke.bennett@mdrx.tech>
2024-05-11 17:42:46 +0100

    chore: dev lazy

--- neovim/neovim-dotfiles/lazy.lua
+++ neovim/neovim-dotfiles/lazy.lua
@@ -15,9 +15,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
 	spec = {
-		-- Preload
 		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-		-- LazyVim
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.go" },
@@ -28,20 +26,21 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
-		{ import = "lazyvim.plugins.extras.lang.swift" },
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
-		-- Overrides
 		{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 		{ "akinsho/bufferline.nvim", enabled = false },
 
-		{
-			"williamboman/mason.nvim",
-			opts = { registries = {
-				"file:~/workspace/personal/contributions/mason-registry",
-			} },
-		},
+		-- Dev
+
+		-- { import = "lazyvim.plugins.extras.lang.swift" },
+		-- {
+		-- 	"williamboman/mason.nvim",
+		-- 	opts = { registries = {
+		-- 		"file:~/workspace/personal/contributions/mason-registry",
+		-- 	} },
+		-- },
 	},
 	defaults = {
 		lazy = false,
