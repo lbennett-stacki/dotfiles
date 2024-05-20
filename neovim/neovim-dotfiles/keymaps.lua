c0045937550c23381ca9e29b7db9a9fd0f38e044
Luke Bennett <luke.bennett@mdrx.tech>
2024-05-20 11:21:24 +0100

    docs: add descriptions

--- neovim/neovim-dotfiles/keymaps.lua
+++ neovim/neovim-dotfiles/keymaps.lua
@@ -1,27 +1,29 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Up>", "<Nop>")
-vim.keymap.set("n", "<Down>", "<Nop>")
-vim.keymap.set("n", "<Left>", "<Nop>")
-vim.keymap.set("n", "<Right>", "<Nop>")
+vim.keymap.set("n", "<Up>", "<Nop>", { desc = "Disable up arrow" })
+vim.keymap.set("n", "<Down>", "<Nop>", { desc = "Disable down arrow" })
+vim.keymap.set("n", "<Left>", "<Nop>", { desc = "Disable left arrow" })
+vim.keymap.set("n", "<Right>", "<Nop>", { desc = "Disable right arrow" })
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- Delete selection
-vim.keymap.set("x", "<leader>p", [["_dP]]) -- Move selection before cursor
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete selection" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Move selection before cursor" })
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Yank selection to system clipboard
-vim.keymap.set("n", "<leader>Y", [["+Y]]) -- Yank line to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank selection to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Move line to system clipboard" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move line down, comply with identation
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move line up, comply with identation
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down, comply with identation" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up, comply with identation" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Page down, cursor in center page
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Page up, cursor in center page
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down, cursor in center page" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up, cursor in center page" })
 
-vim.keymap.set("n", "J", "mzJ`z") -- Collapse next line onto current line, place cursor at end
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Collapse next line onto current line, place cursor at end" })
 
-vim.keymap.set("n", "n", "nzzzv") -- Next search occurance, cursor in center page
-vim.keymap.set("n", "N", "Nzzzv") -- Previous search occurance, cursor in center page
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search occurance, cursor in center page" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search occurance, cursor in center page" })
 
 vim.keymap.set("n", "<leader>dd", function()
 	vim.diagnostic.open_float()
-end, { noremap = true })
+end, { noremap = true, desc = "Open diagnostic float" })
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
